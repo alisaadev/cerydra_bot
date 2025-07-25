1e02832afbb1d1c9548bae70e4dacae0d09cda2b
alisaadev <alisaadev@gmail.com>
2025-07-25 20:51:49 +0800

    Cerydra my wife

--- storage/config.js
+++ storage/config.js
@@ -1,38 +1,38 @@
-import fs from "fs"
-import chalk from "chalk"
-import { fileURLToPath } from "url"
-import junction from "../system/lib/function.js"
+import fs from "fs";
+import chalk from "chalk";
+import { fileURLToPath } from "url";
+import junction from "../system/lib/function.js";
 
 //—————「 Setings your bot 」—————//
-global.name = "Cerydra - Bot"
-global.wm = "Cerydra my wife"
+global.name = "Cerydra - Bot";
+global.wm = "Cerydra my wife";
 
-global.author = "Alisa"
-global.packname = "Created Sticker By"
-global.link = "https://github.com/alisaadev"
+global.author = "Alisa";
+global.packname = "Created Sticker By";
+global.link = "https://github.com/alisaadev";
 
-global.owner = ["6287760363490"]
-global.pairingNumber = "6283872712735"
+global.owner = ["6287760363490"];
+global.pairingNumber = "6283872712735";
 
-global.thumbnail = fs.readFileSync("./storage/crydr.jpg")
-global.prefix = "."
-global.multiplier = 69
-global.func = junction 
+global.thumbnail = fs.readFileSync("./storage/crydr.jpg");
+global.prefix = ".";
+global.multiplier = 69;
+global.func = junction;
 
 //—————「 Message settings 」—————//
 global.mess = {
-    admin: "Perintah ini hanya untuk admin grup",
-    botAdmin: "Perintah ini hanya dapat digunakan bila bot adalah admin",
-    group: "Perintah ini hanya dapat digunakan dalam chat grup",
-    loading: "Silakan tunggu sebentar",
-    private: "Perintah ini hanya dapat digunakan dalam chat pribadi",
-    owner: "Perintah ini hanya untuk pemilik bot"
-}
+  admin: "Perintah ini hanya untuk admin grup",
+  botAdmin: "Perintah ini hanya dapat digunakan bila bot adalah admin",
+  group: "Perintah ini hanya dapat digunakan dalam chat grup",
+  loading: "Silakan tunggu sebentar",
+  private: "Perintah ini hanya dapat digunakan dalam chat pribadi",
+  owner: "Perintah ini hanya untuk pemilik bot",
+};
 
 //—————「 Don"t change it 」—————//
-let file = fileURLToPath(import.meta.url)
+let file = fileURLToPath(import.meta.url);
 fs.watchFile(file, () => {
-    fs.unwatchFile(file)
-    console.log(chalk.redBright("Update config.js"))
-    import(`${file}?update=${Date.now()}`)
-})
+  fs.unwatchFile(file);
+  console.log(chalk.redBright("Update config.js"));
+  import(`${file}?update=${Date.now()}`);
+});
--- system/handler.js
+++ system/handler.js
@@ -1,180 +1,203 @@
-import chalk from "chalk"
-import { promisify } from "util"
-import { fileURLToPath } from "url"
-import { createRequire } from "module"
-import cp, { exec as _exec } from "child_process"
+import chalk from "chalk";
+import { promisify } from "util";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+import cp, { exec as _exec } from "child_process";
 
-const isNumber = x => typeof x === "number" && !isNaN(x)
-const database = (new (await import("./lib/database.js")).default())
+const isNumber = (x) => typeof x === "number" && !isNaN(x);
+const database = new (await import("./lib/database.js")).default();
 
 export async function handler(conn, m, chatUpdate) {
-    if (!m) return
-    if (db == null) await database.write(db)
+  if (!m) return;
+  if (db == null) await database.write(db);
 
-    try {
-        m.exp = 0
-        m.limit = false
+  try {
+    m.exp = 0;
+    m.limit = false;
 
-        await (await import("./lib/loadDatabase.js")).default(m)
+    await (await import("./lib/loadDatabase.js")).default(m);
 
-        if (m.isBaileys) return
-        if (!m.isOwner && db.settings.self) return
-        if (db.settings.autoread) conn.readMessages([m.key])
-        
-        if (m.isOwner) {
-            if (m.body.toLowerCase().startsWith("eval")) {
-                let __dirname = func.path.dirname(fileURLToPath(import.meta.url))
-                let require = createRequire(__dirname), _return = ""
+    if (m.isBaileys) return;
+    if (!m.isOwner && db.settings.self) return;
+    if (db.settings.autoread) conn.readMessages([m.key]);
 
-                try {
-                    _return = /await/i.test(m.text) ? eval("(async() => { " + m.text + " })()") : eval(m.text)
-                } catch (e) {
-                    _return = e
-                }
+    if (m.isOwner) {
+      if (m.body.toLowerCase().startsWith("eval")) {
+        let __dirname = func.path.dirname(fileURLToPath(import.meta.url));
+        let require = createRequire(__dirname),
+          _return = "";
 
-                new Promise((resolve, reject) => {
-                    try {
-                        resolve(_return)
-                    } catch (err) {
-                        reject(err)
-                    }
-                })?.then((res) => m.reply(func.format(res)))?.catch((err) => m.reply(func.format(err)))
-            }
-
-            if (m.body.toLowerCase().startsWith("exec")) {
-                let exec = promisify(_exec).bind(cp)
-                try {
-                    exec(m.text, async (err, stdout) => {
-                        if (err) return m.reply(func.format(err))
-                        if (stdout) return m.reply(func.format(stdout))
-                    })
-                } catch (e) {
-                    m.reply(func.format(e))
-                }
-            }
+        try {
+          _return = /await/i.test(m.text)
+            ? eval("(async() => { " + m.text + " })()")
+            : eval(m.text);
+        } catch (e) {
+          _return = e;
         }
 
-        m.exp += Math.ceil(Math.random() * 10)
-        let user = db.users && db.users[m.sender]
+        new Promise((resolve, reject) => {
+          try {
+            resolve(_return);
+          } catch (err) {
+            reject(err);
+          }
+        })
+          ?.then((res) => m.reply(func.format(res)))
+          ?.catch((err) => m.reply(func.format(err)));
+      }
 
-        for (let name in global.plugins) {
-            let plugin = global.plugins[name]
-
-            if (!plugin) continue
-            if (typeof plugin.all === "function") {
-                try {
-                    await plugin.all.call(conn, m, { chatUpdate })
-                } catch (e) {
-                    console.error(e)
-                    conn.sendMessage(owner[0] + "@s.whatsapp.net", { text: `👋🏻 Hello developer, ada yang error nih!\n\nCmd: ${m.text}\n\n${func.format(e)}`, mentions: [m.sender] })
-                }
-            }
-
-            if (typeof plugin.before === "function") {
-                if (await plugin.before.call(conn, m, { chatUpdate }))
-                continue
-            }
-
-            if (m.prefix) {
-                let { args, command, text } = m
-                let isAccept = Array.isArray(plugin.command) ? plugin.command.some(cmd => cmd === command) : false
-
-                m.plugin = name
-                if (!isAccept) continue
-                if (plugin.owner && !m.isOwner) {
-                    m.reply(mess.owner)
-                    continue
-                }
-
-                if (plugin.group && !m.isGroup) {
-                    m.reply(mess.group)
-                    continue
-                }
-
-                if (plugin.botAdmin && !m.isBotAdmin) {
-                    m.reply(mess.botAdmin)
-                    continue
-                }
-
-                if (plugin.admin && !m.isAdmin) {
-                    m.reply(mess.admin)
-                    continue
-                }
-
-                if (plugin.private && m.isGroup) {
-                    m.reply(mess.private)
-                    continue
-                }
-
-                m.isCommand = true
-                let extra = {
-                    conn,
-                    args,
-                    command,
-                    text,
-                    chatUpdate
-                }
-
-                try {
-                    await plugin.run(m, extra)
-                } catch (e) {
-                    console.error(e)
-                    conn.sendMessage(owner[0] + "@s.whatsapp.net", { text: `👋🏻 Hello developer, ada yang error nih!\n\nCmd: ${m.text}\n\n${func.format(e)}`, mentions: [m.sender] })
-                } finally {
-                    if (typeof plugin.after === "function") {
-                        try {
-                            await plugin.after.call(conn, m, extra)
-                        } catch (e) {
-                            console.error(e)
-                            conn.sendMessage(owner[0] + "@s.whatsapp.net", { text: `👋🏻 Hello developer, ada yang error nih!\n\nCmd: ${m.text}\n\n${func.format(e)}`, mentions: [m.sender] })
-                        }
-                    }
-                }
-            }
+      if (m.body.toLowerCase().startsWith("exec")) {
+        let exec = promisify(_exec).bind(cp);
+        try {
+          exec(m.text, async (err, stdout) => {
+            if (err) return m.reply(func.format(err));
+            if (stdout) return m.reply(func.format(stdout));
+          });
+        } catch (e) {
+          m.reply(func.format(e));
         }
-    } catch (e) {
-        console.error(e)
+      }
     }
+
+    m.exp += Math.ceil(Math.random() * 10);
+    let user = db.users && db.users[m.sender];
+
+    for (let name in global.plugins) {
+      let plugin = global.plugins[name];
+
+      if (!plugin) continue;
+      if (typeof plugin.all === "function") {
+        try {
+          await plugin.all.call(conn, m, { chatUpdate });
+        } catch (e) {
+          console.error(e);
+          conn.sendMessage(owner[0] + "@s.whatsapp.net", {
+            text: `👋🏻 Hello developer, ada yang error nih!\n\nCmd: ${m.text}\n\n${func.format(e)}`,
+            mentions: [m.sender],
+          });
+        }
+      }
+
+      if (typeof plugin.before === "function") {
+        if (await plugin.before.call(conn, m, { chatUpdate })) continue;
+      }
+
+      if (m.prefix) {
+        let { args, command, text } = m;
+        let isAccept = Array.isArray(plugin.command)
+          ? plugin.command.some((cmd) => cmd === command)
+          : false;
+
+        m.plugin = name;
+        if (!isAccept) continue;
+        if (plugin.owner && !m.isOwner) {
+          m.reply(mess.owner);
+          continue;
+        }
+
+        if (plugin.group && !m.isGroup) {
+          m.reply(mess.group);
+          continue;
+        }
+
+        if (plugin.botAdmin && !m.isBotAdmin) {
+          m.reply(mess.botAdmin);
+          continue;
+        }
+
+        if (plugin.admin && !m.isAdmin) {
+          m.reply(mess.admin);
+          continue;
+        }
+
+        if (plugin.private && m.isGroup) {
+          m.reply(mess.private);
+          continue;
+        }
+
+        m.isCommand = true;
+        let extra = {
+          conn,
+          args,
+          command,
+          text,
+          chatUpdate,
+        };
+
+        try {
+          await plugin.run(m, extra);
+        } catch (e) {
+          console.error(e);
+          conn.sendMessage(owner[0] + "@s.whatsapp.net", {
+            text: `👋🏻 Hello developer, ada yang error nih!\n\nCmd: ${m.text}\n\n${func.format(e)}`,
+            mentions: [m.sender],
+          });
+        } finally {
+          if (typeof plugin.after === "function") {
+            try {
+              await plugin.after.call(conn, m, extra);
+            } catch (e) {
+              console.error(e);
+              conn.sendMessage(owner[0] + "@s.whatsapp.net", {
+                text: `👋🏻 Hello developer, ada yang error nih!\n\nCmd: ${m.text}\n\n${func.format(e)}`,
+                mentions: [m.sender],
+              });
+            }
+          }
+        }
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export async function participantsUpdate({ id, participants, action }) {
-    if (db.settings.self) return
-    if (db == null) await database.write(db)
+  if (db.settings.self) return;
+  if (db == null) await database.write(db);
 
-    let chat = db.chats[id] || {}, ppuser
-    let metadata = await conn.groupMetadata(id)
+  let chat = db.chats[id] || {},
+    ppuser;
+  let metadata = await conn.groupMetadata(id);
 
-    switch (action) {
-        case "add":
-        case "remove":
-            if (chat.welcome) {
-                for (let user of participants) {
-                    try {
-                        ppuser = await conn.profilePictureUrl(user, "image")
-					} catch {
-                        ppuser = "https://telegra.ph/file/04022fa475e4162862d8b.jpg"
-					} finally {
-                        let tekswell = `Halo @${user.split("@")[0]} 👋\n\nSelamat datang di grup ${metadata.subject}! Kami senang kamu bergabung dengan kami.\n\nSaya harap kamu betah disini dan jangan lupa untuk selalu mengikuti peraturan yang ada`  
-                        let teksbye = `Selamat tinggal @${user.split("@")[0]} 👋\n\nSalam perpisahan, kami harap kamu baik-baik saja disana`
+  switch (action) {
+    case "add":
+    case "remove":
+      if (chat.welcome) {
+        for (let user of participants) {
+          try {
+            ppuser = await conn.profilePictureUrl(user, "image");
+          } catch {
+            ppuser = "https://telegra.ph/file/04022fa475e4162862d8b.jpg";
+          } finally {
+            let tekswell = `Halo @${user.split("@")[0]} 👋\n\nSelamat datang di grup ${metadata.subject}! Kami senang kamu bergabung dengan kami.\n\nSaya harap kamu betah disini dan jangan lupa untuk selalu mengikuti peraturan yang ada`;
+            let teksbye = `Selamat tinggal @${user.split("@")[0]} 👋\n\nSalam perpisahan, kami harap kamu baik-baik saja disana`;
 
-                        if (action == "add") {
-                        	conn.sendMessage(id, { image: { url: ppuser }, contextInfo: { mentionedJid: [user] }, caption: tekswell, mentions: [user] })
-                        } else if (action == "remove") {
-                        	conn.sendMessage(id, { text: teksbye, mentions: [user] })
-                        }
-                    }
-                }
+            if (action == "add") {
+              conn.sendMessage(id, {
+                image: { url: ppuser },
+                contextInfo: { mentionedJid: [user] },
+                caption: tekswell,
+                mentions: [user],
+              });
+            } else if (action == "remove") {
+              conn.sendMessage(id, { text: teksbye, mentions: [user] });
             }
-            break
-        case "promote":
-        case "demote":
-        	let tekspro = `Selamat @${participants[0].split("@")[0]} atas kenaikan pangkatnya di grup ${metadata.subject} 🥂`
-        	let teksdem = `Sabar yaa @${participants[0].split("@")[0]} atas penurunan pangkatnya di grup ${metadata.subject} 😔`
+          }
+        }
+      }
+      break;
+    case "promote":
+    case "demote":
+      let tekspro = `Selamat @${participants[0].split("@")[0]} atas kenaikan pangkatnya di grup ${metadata.subject} 🥂`;
+      let teksdem = `Sabar yaa @${participants[0].split("@")[0]} atas penurunan pangkatnya di grup ${metadata.subject} 😔`;
 
-            if (chat.detect) {
-                if (action == "promote") conn.sendMessage(id, { text: tekspro, mentions: [participants[0]] })
-                if (action == "demote") conn.sendMessage(id, { text: teksdem, mentions: [participants[0]] })
-            }
-            break
-    }
+      if (chat.detect) {
+        if (action == "promote")
+          conn.sendMessage(id, { text: tekspro, mentions: [participants[0]] });
+        if (action == "demote")
+          conn.sendMessage(id, { text: teksdem, mentions: [participants[0]] });
+      }
+      break;
+  }
 }
